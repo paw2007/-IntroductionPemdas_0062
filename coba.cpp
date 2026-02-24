eb0eba561f4e2ff30f158da5b0999bc0398a8fee
paw2007 <azfa4807@gmail.com>
2026-02-24 16:13:39 +0700

    Add files via upload

---
+++ coba.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool cek = true;
+   char jk;
+   int angka = 10;
+   float tinggi = 100.5;
+   double harga = 200000.00;
+   string nama = "Fawwaz";
+
+   jk = 'L';
+   cout << "selamat datang" << endl;
+   cout << "DI TI UMY\n";
+   cout << "Yogyakarta\n";
+   nama = "Fawwaz";
+   cout << "nama saya : " << nama ;
+}
